82f70168b4930a5483de6b261f4c21ad801ee46d
Jesus Rodriguez <foxandxss@gmail.com>
2014-10-27 12:21:54 +0100

    :yum:

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular - Laravel4 Workflow
+
+WIP
---
+++ gulpfile.js
@@ -0,0 +1,179 @@
+var gulp               = require('gulp');
+var fs                 = require('fs');
+var plugins            = require('gulp-load-plugins')();
+var es                 = require('event-stream');
+var historyApiFallback = require('connect-history-api-fallback');
+
+var handlebarOpts = {
+  helpers: {
+        assetPath: function (path, context) {
+            return context.data.root[path];
+        }
+    }
+};
+
+var paths = {
+  appJavascript:    ['app/js/app.js', 'app/js/**/*.js'],
+  appTemplates:     'app/js/**/*.tpl.html',
+  appMainSass:      'app/scss/main.scss',
+  appStyles:        'app/scss/**/*.scss',
+  appImages:        'app/images/**/*',
+  indexHbs:         'app/index.hbs',
+  vendorJavascript: ['vendor/js/angular.js', 'vendor/js/**/*.js'],
+  vendorCss:        ['vendor/css/**/*.css'],
+  finalAppJsPath:   '/js/app.js',
+  finalAppCssPath:  '/css/app.css',
+  specFolder:       ['spec/**/*_spec.js'],
+  tmpFolder:        'tmp',
+  tmpJavascript:    'tmp/js',
+  tmpAppJs:         'tmp/js/app.js',
+  tmpCss:           'tmp/css',
+  tmpImages:        'tmp/images',
+  distFolder:       'dist',
+  distJavascript:   'dist/js',
+  distCss:          'dist/css',
+  distImages:       'dist/images',
+  distJsManifest:   'dist/js/rev-manifest.json',
+  distCssManifest:  'dist/css/rev-manifest.json'
+};
+
+gulp.task('scripts-dev', function() {
+  return gulp.src(paths.vendorJavascript.concat(paths.appJavascript, paths.appTemplates))
+    .pipe(plugins.if(/html$/, buildTemplates()))
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.concat('app.js'))
+    .pipe(plugins.sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.tmpJavascript))
+    .pipe(plugins.connect.reload());
+});
+gulp.task('scripts-prod', function() {
+  return gulp.src(paths.vendorJavascript.concat(paths.appJavascript, paths.appTemplates))
+    .pipe(plugins.if(/html$/, buildTemplates()))
+    .pipe(plugins.concat('app.js'))
+    .pipe(plugins.ngAnnotate())
+    .pipe(plugins.uglify())
+    .pipe(plugins.rev())
+    .pipe(gulp.dest(paths.distJavascript))
+    .pipe(plugins.rev.manifest({path: 'rev-manifest.json'}))
+    .pipe(gulp.dest(paths.distJavascript));
+});
+
+gulp.task('styles-dev', function() {
+  return gulp.src(paths.vendorCss.concat(paths.appMainSass))
+    .pipe(plugins.if(/scss$/, plugins.sass()))
+    .pipe(plugins.concat('app.css'))
+    .pipe(gulp.dest(paths.tmpCss))
+    .pipe(plugins.connect.reload());
+});
+
+gulp.task('styles-prod', function() {
+  return gulp.src(paths.vendorCss.concat(paths.appMainSass))
+    .pipe(plugins.if(/scss$/, plugins.sass()))
+    .pipe(plugins.concat('app.css'))
+    .pipe(plugins.minifyCss())
+    .pipe(plugins.rev())
+    .pipe(gulp.dest(paths.distCss))
+    .pipe(plugins.rev.manifest({path: 'rev-manifest.json'}))
+    .pipe(gulp.dest(paths.distCss));
+});
+
+gulp.task('images-dev', function() {
+  return gulp.src(paths.appImages)
+    .pipe(gulp.dest(paths.tmpImages))
+    .pipe(plugins.connect.reload());
+});
+
+gulp.task('images-prod', function() {
+  return gulp.src(paths.appImages)
+    .pipe(gulp.dest(paths.distImages));
+});
+
+gulp.task('indexHtml-dev', ['scripts-dev', 'styles-dev'], function() {
+  var manifest = {};
+
+  manifest[paths.finalAppJsPath]  = paths.finalAppJsPath;
+  manifest[paths.finalAppCssPath] = paths.finalAppCssPath;
+
+  return gulp.src(paths.indexHbs)
+    .pipe(plugins.compileHandlebars(manifest, handlebarOpts))
+    .pipe(plugins.rename('index.html'))
+    .pipe(gulp.dest(paths.tmpFolder))
+    .pipe(plugins.connect.reload());
+});
+
+gulp.task('indexHtml-prod', ['scripts-prod', 'styles-prod'], function() {
+  var jsManifest  = JSON.parse(fs.readFileSync(paths.distJsManifest, 'utf8'));
+  var cssManifest = JSON.parse(fs.readFileSync(paths.distCssManifest, 'utf8'));
+
+  var manifest = {};
+
+  manifest[paths.finalAppJsPath]  = jsManifest['app.js'];
+  manifest[paths.finalAppCssPath] = cssManifest['app.css'];
+
+  return gulp.src(paths.indexHbs)
+    .pipe(plugins.compileHandlebars(manifest, handlebarOpts))
+    .pipe(plugins.rename('index.html'))
+    .pipe(gulp.dest(paths.distFolder));
+});
+
+gulp.task('lint', function() {
+  return gulp.src(paths.appJavascript.concat(paths.specFolder))
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('testem', function() {
+  return gulp.src(['']) // We don't need files, that is managed on testem.json
+    .pipe(plugins.testem({
+      configFile: 'testem.json'
+    }));
+});
+
+gulp.task('clean', function() {
+  return gulp.src([paths.tmpFolder, paths.distFolder], {read: false})
+    .pipe(plugins.rimraf());
+});
+
+gulp.task('watch', ['webserver'], function() {
+  gulp.watch(paths.appJavascript, ['lint', 'scripts']);
+  gulp.watch(paths.appTemplates, ['scripts']);
+  gulp.watch(paths.vendorJavascript, ['scripts']);
+  gulp.watch(paths.appImages, ['images']);
+  gulp.watch(paths.specFolder, ['lint']);
+  gulp.watch(paths.indexHtml, ['indexHtml']);
+  gulp.watch(paths.appStyles, ['styles']);
+  gulp.watch(paths.vendorCss, ['styles']);
+});
+
+gulp.task('webserver', ['indexHtml-dev', 'images-dev'], function() {
+  plugins.connect.server({
+    root: paths.tmpFolder,
+    port: 5000,
+    livereload: true,
+    middleware: function(connect, o) {
+        return [ (function() {
+            var url = require('url');
+            var proxy = require('proxy-middleware');
+            var options = url.parse('http://localhost:8080/api');
+            options.route = '/api';
+            return proxy(options);
+        })(), historyApiFallback ];
+    }
+  });
+});
+
+gulp.task('default', ['watch']);
+gulp.task('production', ['scripts-prod', 'styles-prod', 'images-prod', 'indexHtml-prod']);
+
+function buildTemplates() {
+  return es.pipeline(
+    plugins.minifyHtml({
+      empty: true,
+      spare: true,
+      quotes: true
+    }),
+    plugins.angularTemplatecache({
+      module: 'app'
+    })
+  );
+}
